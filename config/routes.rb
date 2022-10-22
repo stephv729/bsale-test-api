17a6b7088e4cc6e0956d6c5517eb6b900c41cc0e
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-22 18:15:05 -0500

    Add: products_by_category to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :categories
+  resources :products, only: %i[index show]
+  resources :categories, only: %i[index]
+  get "products_by_category", :to => "products#index_by_category"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
