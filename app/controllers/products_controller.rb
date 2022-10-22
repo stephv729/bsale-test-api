3bf8617c5d0936b9e316cd2d59288280c016dc60
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-22 18:10:30 -0500

    Remove: comments from products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,37 +13,6 @@ class ProductsController < ApplicationController
     render json: @product
   end
 
-  def index_by_category
-    @products = Product.where(category: product_params[:category])
-
-    render json: @products
-  end
-
-  # # POST /products
-  # def create
-  #   @product = Product.new(product_params)
-
-  #   if @product.save
-  #     render json: @product, status: :created, location: @product
-  #   else
-  #     render json: @product.errors, status: :unprocessable_entity
-  #   end
-  # end
-
-  # # PATCH/PUT /products/1
-  # def update
-  #   if @product.update(product_params)
-  #     render json: @product
-  #   else
-  #     render json: @product.errors, status: :unprocessable_entity
-  #   end
-  # end
-
-  # # DELETE /products/1
-  # def destroy
-  #   @product.destroy
-  # end
-
   private
 
   # Use callbacks to share common setup or constraints between actions.
