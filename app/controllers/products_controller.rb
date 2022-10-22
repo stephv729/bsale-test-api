669b82098d78e3f9acd0543e05edaac2b5206a0c
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-22 17:44:20 -0500

    Remove: create,update and delete methods in controllers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,39 +13,46 @@ class ProductsController < ApplicationController
     render json: @product
   end
 
-  # POST /products
-  def create
-    @product = Product.new(product_params)
+  def index_by_category
+    @products = Product.where(category: product_params[:category])
 
-    if @product.save
-      render json: @product, status: :created, location: @product
-    else
-      render json: @product.errors, status: :unprocessable_entity
-    end
+    render json: @products
   end
 
-  # PATCH/PUT /products/1
-  def update
-    if @product.update(product_params)
-      render json: @product
-    else
-      render json: @product.errors, status: :unprocessable_entity
-    end
-  end
+  # # POST /products
+  # def create
+  #   @product = Product.new(product_params)
 
-  # DELETE /products/1
-  def destroy
-    @product.destroy
-  end
+  #   if @product.save
+  #     render json: @product, status: :created, location: @product
+  #   else
+  #     render json: @product.errors, status: :unprocessable_entity
+  #   end
+  # end
+
+  # # PATCH/PUT /products/1
+  # def update
+  #   if @product.update(product_params)
+  #     render json: @product
+  #   else
+  #     render json: @product.errors, status: :unprocessable_entity
+  #   end
+  # end
+
+  # # DELETE /products/1
+  # def destroy
+  #   @product.destroy
+  # end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def product_params
-      params.require(:product).permit(:name, :url_image, :price, :discount, :category)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def product_params
+    params.require(:product).permit(:name, :url_image, :price, :discount, :category)
+  end
 end
