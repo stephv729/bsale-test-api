ff4dc03d52d874d1bfafcac3b2d782ddffba448e
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-22 19:52:57 -0500

    Change: config in production env to not precompile assets(since is an API app)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,9 +26,6 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
-
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
