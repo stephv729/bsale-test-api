4308418db17fa2c8631516d5c80caac13243245c
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-23 16:40:38 -0500

    Add: products method to categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,27 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[products]
   # GET /categories
   def index
     @categories = Category.all
 
     render json: @categories
+  end
+
+  def products
+    @products = @category.products
+
+    render json: @products
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
